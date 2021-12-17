88dd9611250d2063e4eafe2d01f479238694c120
ziMu612 <1370658347@qq.com>
2021-12-17 18:00:36 +0800

    login and register 1.0

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入icon
+import './assets/font/iconfont.css'
+// import axios from 'axios'
+
+// Vue.prototype.$axios = axios
+// 配置请求的根路径
+// axios.defaults.baseURL = ''
 
 Vue.config.productionTip = false
 
