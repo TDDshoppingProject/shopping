5bcb981762032d77f2f26a7364eabd3b9687c1f7
huangjingping520 <6109119167@email.ncu.edu.cn>
2021-12-20 17:40:08 +0800

    轮播图以及侧边广告

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,12 +3,17 @@
     <el-row class="header">
       <el-col :span="8">
         <div class="my-info">
-          <el-link :underline="false"><i class="el-icon-user-solid" style="color: #EC9454;"></i>我的淘多多</el-link>
+          <el-link :underline="false"
+            ><i class="el-icon-user-solid" style="color: #ec9454"></i
+            >我的淘多多</el-link
+          >
         </div>
       </el-col>
       <el-col :span="16">
         <div class="login-sign">
-          <el-link :underline="false" type="warning" class="shopping-car"><i class="el-icon-shopping-cart-2"></i>购物车</el-link>
+          <el-link :underline="false" type="warning" class="shopping-car"
+            ><i class="el-icon-shopping-cart-2"></i>购物车</el-link
+          >
           <el-link :underline="false" type="danger">请登录</el-link>
           <el-link :underline="false" class="sign">免费注册</el-link>
         </div>
@@ -17,21 +22,51 @@
     <el-row class="logo-search">
       <el-col :span="6">
         <div class="logo">
-          <img src="../assets/images/logo_transparent.png" alt="">
+          <img src="../assets/images/logo_transparent.png" alt="" />
         </div>
       </el-col>
       <el-col :span="18">
         <div class="search">
-          <el-input placeholder="请输入内容" v-model="input" class="input-with-select">
-    <el-select v-model="select" slot="prepend" placeholder="请选择">
-      <el-option label="宝贝" value="1"></el-option>
-      <el-option label="店铺" value="2"></el-option>
-    </el-select>
-    <el-button slot="append" icon="el-icon-search">搜索</el-button>
-  </el-input>
+          <el-input
+            placeholder="请输入内容"
+            v-model="input"
+            class="input-with-select"
+          >
+            <el-select v-model="select" slot="prepend" placeholder="请选择">
+              <el-option label="宝贝" value="1"></el-option>
+              <el-option label="店铺" value="2"></el-option>
+            </el-select>
+            <el-button slot="append" icon="el-icon-search">搜索</el-button>
+          </el-input>
         </div>
       </el-col>
     </el-row>
+    <div class="mainBox">
+      <el-container>
+        <el-main style="margin-left: -250px; margin-top: -10px;">
+          <div class="block">
+            <el-carousel trigger="click" height="400px">
+              <el-carousel-item v-for="item in imgDate" :key="item">
+                <img
+                  :src="item.url"
+                  width="100%"
+                  height="100%"
+                  object-fit="cover"
+                />
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+        </el-main>
+        <el-aside width="450px">
+          <div class="ad">
+            这里是广告
+          </div>
+        </el-aside>
+      </el-container>
+    </div>
+    <div class="adBox">
+        推荐目录
+    </div>
   </div>
 </template>
 
@@ -41,16 +76,29 @@ export default {
   data () {
     return {
       input: '',
-      select: ''
-    }
+      select: '',
+      imgDate: [
+        {
+          url: require('../assets/images/1.jpeg')
+        },
+        {
+          url: require('../assets/images/2.jpeg')
+        },
+        {
+          url: require('../assets/images/3.jpeg')
+        },
+        {
+          url: require('../assets/images/4.jpeg')
+        }
+      ]
+    };
   }
-
 };
 </script>
 
 <style scoped>
 .welcome-container {
-  background-color: #EAE8E8;
+  background-color: #eae8e8;
   height: 100%;
 }
 .el-col {
@@ -58,7 +106,7 @@ export default {
 }
 .header {
   height: 30px;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
 }
 .my-info {
   line-height: 30px;
@@ -71,7 +119,7 @@ export default {
 .shopping-car {
   margin-right: 30px;
 }
-.sign{
+.sign {
   margin-left: 5px;
   margin-right: 130px;
 }
@@ -91,7 +139,7 @@ export default {
 .el-input {
   width: 500px;
   margin-left: 100px;
-  border: 1px solid #EC9454;
+  border: 1px solid #ec9454;
   border-radius: 30px;
 }
 
@@ -110,5 +158,37 @@ export default {
   border-bottom-right-radius: 30px;
   width: 60px;
   background-color: #ec7a29;
+}
+
+.mainBox {
+  margin: 5px 120px;
+  border-radius: 20px;
+  background-color: #fff;
+}
+.block {
+  width: 700px;
+  padding: 10px;
+}
+.el-carousel__item h3 {
+  border-radius: 20px;
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+.ad {
+  margin-top: 20px;
+  margin-right: 20px;
+  padding: 10px;
+  border-radius: 20px;
+  height: 380px;
+  background-color: #f4f4f4;
+}
+
+.adBox {
+  margin: 5px 120px;
+  border-radius: 20px;
+  background-color: #fff;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Row, Col, Link, Icon, Input, Select, Option } from 'element-ui'
+import { Button, Row, Col, Link, Icon, Input, Select, Option, Carousel, CarouselItem, Container, Aside, Main, Header, Footer } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Row)
@@ -9,3 +9,10 @@ Vue.use(Icon)
 Vue.use(Input)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Header)
+Vue.use(Footer)
